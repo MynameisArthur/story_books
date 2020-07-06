dde6d4c9ed8840fb0b2a4089ea654a8b051c06ef
Arthur <arthurd800@wp.pl>
2020-07-06 09:33:00 +0200

    added handlebars template engine

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 const app = express();
+
+//Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+//Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', ' .hbs');
+
 //Load config
 dotenv.config({ path: './config/config.env' });
 connectDB();
