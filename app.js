c959fe56a846c0b2d09419d71621158aa9cf8b7f
Arthur <arthurd800@wp.pl>
2020-07-06 11:58:55 +0200

    implementing authentication with passport.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 
@@ -12,13 +15,29 @@ if (process.env.NODE_ENV === 'development') {
 }
 //Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', ' .hbs');
+app.set('view engine', '.hbs');
+//Static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Routes
 app.use('/', require('./routes/index'));
+app.use('/', require('./routes/auth'));
 
 //Load config
 dotenv.config({ path: './config/config.env' });
+//Passport config
+require('./config/passport')(passport);
+
+//Sessions
+app.use(session({
+    secret: 'monday',
+    resave: false,
+    saveUninitialized: false
+}));
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 connectDB();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
