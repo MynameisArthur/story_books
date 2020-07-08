ec025ae1738da3f90eb147c34d56eb3ce07e18c3
Arthur <arthurd800@wp.pl>
2020-07-08 08:16:35 +0200

    added date formating in dashboard table

--- app.js
+++ app.js
@@ -42,25 +42,25 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars Helpers
-// const {
-//     formatDate,
-//     stripTags,
-//     truncate,
-//     editIcon,
-//     select,
-// } = require('./helpers/hbs');
+const {
+    formatDate,
+    // stripTags,
+    // truncate,
+    // editIcon,
+    // select,
+} = require('./helpers/hbs');
 
 // Handlebars
 app.engine(
     '.hbs',
     exphbs({
-        // helpers: {
-        //     formatDate,
-        //     stripTags,
-        //     truncate,
-        //     editIcon,
-        //     select,
-        // },
+        helpers: {
+            formatDate,
+            // stripTags,
+            // truncate,
+            // editIcon,
+            // select,
+        },
         defaultLayout: 'main',
         extname: '.hbs',
     })
